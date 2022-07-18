7e0eae25bceeffd29bfb06c5746235ef3dfa1809
cortiz642 <chris.o.ortiz93@gmail.com>
2022-07-17 20:03:30 -0500

    added student and studenttest classes to include constructors

---
+++ src/test/java/Student.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+
+public class Student {
+    String name;
+    Integer Id;
+    // returns the student's id
+    public long getId(){
+        return this.Id = Id;
+    }
+
+    // returns the student's name
+    public String getName(){
+
+    }
+
+    // adds the given grade to the grades list
+    public void addGrade(int grade){
+
+    }
+
+    // returns the list of grades
+    public ArrayList<Integer> getGrades(){
+
+    }
+
+    // returns the average of the students grades
+    public double getGradeAverage(){
+
+    }
+}
